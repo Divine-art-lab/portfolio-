0475584bac1917a9fc864ce037215bacc5abcdba
Divine-art-lab <divineij35@gmail.com>
2024-09-16 22:05:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var menu = document.getElementById("nav");
+  if(menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
