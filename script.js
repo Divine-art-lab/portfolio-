ac08292439c1cd49cc6958e1c0119dc35f1b416c
Divine-art-lab <divineij35@gmail.com>
2024-11-21 20:54:08 +0100

    update

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function toggle() {
     menu.style.display = "block";
   }
 }
+function active() {
+  alert('You are back online');
+}
+function offline() {
+  alert('You are currently offline, please trun-on your internet connection or connect to a WiFi');
+}
