0447969406820f2b46857f7c7850f95f8da9c5bb
DivineTech <divineij35@gmail.com>
2025-06-13 15:13:23 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-function toggle() {
-  var menu = document.getElementById("nav");
-  if(menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-}
+
 function active() {
   alert('You are back online');
 }
 function offline() {
   alert('You are currently offline, please trun-on your internet connection or connect to a WiFi');
+}
+function toggleTheme() {
+  document.body.classList.toggle('dark-theme');
 }
