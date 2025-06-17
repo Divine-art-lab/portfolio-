39b05deee727cb9a01470e99851914d392e988a5
Divine-art-lab <divineij35@gmail.com>
2025-06-17 08:52:11 +0100

    update

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-
-function active() {
-  alert('You are back online');
+let alertElement = document.querySelector(".notice-internet-con");
+window.addEventListener("online", () => {
+  alertElement.style.display = "block";
+  alertElement.style.backgroundColor = "green";
+  alertElement.textContent = "you're back online, Enjoy browsing 😊";
+  setTimeout(del, 2000);
+});
+window.addEventListener("offline", () => {
+  alertElement.style.display = "block";
+  alertElement.style.backgroundColor = "red";
+alertElement.textContent = "you're currently offline, please check your internet connection ";
+if (window.online) {
+  del();
 }
-function offline() {
-  alert('You are currently offline, please trun-on your internet connection or connect to a WiFi');
+});
+function del() {
+  alertElement.style.display = "none";
 }
 function toggleTheme() {
   document.body.classList.toggle('dark-theme');
 }
+
